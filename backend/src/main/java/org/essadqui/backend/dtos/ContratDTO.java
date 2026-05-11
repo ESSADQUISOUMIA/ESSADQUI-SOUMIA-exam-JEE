aec48abf2dec3ae73dea92227414d6099da03311
ESSADQUI <soumia.essadqui-etu@etu.univh2c.ma>
2026-05-11 09:48:27 +0100

    third commit

--- backend/src/main/java/org/essadqui/backend/dtos/ContratDTO.java
+++ backend/src/main/java/org/essadqui/backend/dtos/ContratDTO.java
@@ -1,4 +1,29 @@
 package org.essadqui.backend.dtos;
 
+import lombok.*;
+
+@Data @AllArgsConstructor @NoArgsConstructor
 public class ContratDTO {
+    private Long id;
+    private String dateSouscription;
+    private String statut;
+    private double montantCotisation;
+    private int dureeContrat;
+    private double tauxCouverture;
+    private Long clientId;
+    private String clientNom;
+    private String typeContrat; // AUTOMOBILE, HABITATION, SANTE
+
+    // Automobile
+    private String numImmatriculation;
+    private String marque;
+    private String modele;
+
+    // Habitation
+    private String typeLogement;
+    private String adresse;
+    private Double superficie;
+    // Santé
+    private String niveauCouverture;
+    private Integer nbPersonnesCouvertes;
 }
